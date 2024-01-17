bc9336e4880e8ab8cbbdf5c8d9079e5d0ddf1e5d
limeojin363 <2002eojin@gmail.com>
2024-01-17 23:12:42 +0900

    tem: .

---
+++ cpp-examples/my-default-template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void preset() {}
+
+void solve() {}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  preset();
+  solve();
+}
